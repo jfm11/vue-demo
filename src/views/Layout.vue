d5f57d968dee22049619f90c32b5c32c1be50557
fengming.jiang <fengming.jiang@bjyada.com>
2018-12-25 15:56:45 +0800

    增加loading

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,17 +1,25 @@
 <template>
-  <div>
+  <div
+    v-loading.fullscreen="menuList.length === 0"
+    element-loading-text="拼命加载中"
+    element-loading-spinner="el-icon-loading"
+    element-loading-background="rgba(0, 0, 0, 0.8)"
+  >
     <!-- <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <SideBar />
-    <router-view />
+    <router-view v-if="menuList.length > 0" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import SideBar from "@/components/SideBar";
+import { mapState } from "vuex";
 export default {
-  name: "home",
+  computed: {
+    ...mapState("menu", ["menuList"])
+  },
   methods: {},
   components: { SideBar }
 };
